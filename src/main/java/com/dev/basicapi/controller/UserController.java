6502e493c1604bfb25d865fbf9de0576f1818841
Leonardo Souza <leonado_b22_@hotmail.com>
2022-08-22 10:40:14 -0300

    add API documentation
    adicionando o swagger para documentar a API

--- src/main/java/com/dev/basicapi/controller/UserController.java
+++ src/main/java/com/dev/basicapi/controller/UserController.java
@@ -2,6 +2,8 @@ package com.dev.basicapi.controller;
 
 
 import com.dev.basicapi.model.UserModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,34 +14,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import javax.websocket.server.PathParam;
 
+@Api(value = "USER")
 @RestController()
 @RequestMapping("/user")
 public class UserController {
 
+    @ApiOperation(value = "Show all Users")
     @GetMapping()
     public ResponseEntity<?> findAll(){
 
         return ResponseEntity.ok("DONE");
     }
-
+    @ApiOperation(value = "Find user by id")
     @GetMapping("/:id")
     public ResponseEntity<?> findById(@PathParam("id") Long id){
 
         return ResponseEntity.ok("DONE");
     }
-
+    @ApiOperation(value = "Save a user")
     @PostMapping()
     public ResponseEntity<?> save(@RequestBody UserModel user){
 
         return ResponseEntity.ok("DONE");
     }
 
+    @ApiOperation(value = "Update a user")
     @PutMapping()
     public ResponseEntity<?> update(@RequestBody UserModel user){
 
         return ResponseEntity.ok("DONE");
     }
 
+    @ApiOperation(value = "Delete a user")
     @DeleteMapping("/:id")
     public ResponseEntity<?> delete(@PathParam("id") Long id){
 
